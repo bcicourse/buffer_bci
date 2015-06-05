201a04fc767105205fb84269eb9f38d5a302a83a
bcicourse <guest_account@Macbook-K-Dijkstra.local>
2015-06-05 16:05:30 +0200

    fix muse startup stuff

--- dataAcq/startMuse.sh
+++ dataAcq/startMuse.sh
@@ -6,7 +6,7 @@ oscport=1234
 #oscdevice=ffc3
 # 1) run the OSC -> ft_buffer converter with parameters for the MUSE  !in the background!
 #    This will then wait for data from the MUSE and connection to the buffer
-java -cp buffer/java/Buffer.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 6 220 1 10 &
+java -cp buffer/java/BufferClient.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 4 220 1 10 &
 # catch ctrl-c and kill the java too
 trap 'kill %1' SIGTERM SIGINT SIGHUP
 
@@ -26,7 +26,7 @@ else
    export DYLD_LIBRARY_PATH=`pwd`:$DYLD_LIBRARY_PATH
 fi
 # 2) run the muse-io driver, by default search for device with name muse
-$buffexe --preset ab --osc osc.udp://localhost:$oscport
+$buffexe --preset 10 —50hz --osc osc.udp://localhost:$oscport
 if [ $? -ne 0 ]; then
 	 echo "Error couldn't connect to the MUSE"
 	 kill %1 # kill the background java job
